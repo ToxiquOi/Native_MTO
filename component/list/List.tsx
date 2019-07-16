f292f553bbdc65c357d0313b5945942101a9823f
uha-g.vuillemin <gauthier.vuillemin@uha.fr>
2019-07-16 15:14:27 +0200

    speed

--- component/list/List.tsx
+++ component/list/List.tsx
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import { Text, } from 'react-native'
 
-export default class List extends Component {
+interface Props {
+
+}
+
+interface State {
+    city: string,
+    report: object | null
+}
+
+export default class List extends Component<Props, State> {
+
+    state: State = {
+        city: '',
+        report: null
+    }
 
     static navigationOptions = (params) => {
         return {
-            title: `Météo de la ville de ${params.city}`,
+            title: `Météo de la ville de ${this.state.city}`,
         }
     };
 
     constructor(props) {
         super(props);
+        console.log('state', this.props.navigation)
+        this.setState( (state: State) => {
+            state.city = this.props.navigation.state.params.city;
+
+            return state
+        })
     }
 
     render() {
